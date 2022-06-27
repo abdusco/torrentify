dc51b8d0407d1cc4363d9c6ed4f79f83c1df8d87
Abdussamet Kocak <abdus@abdus.dev>
2022-06-27 22:45:47 +0300

    feat: Initial prototype

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/abdusco/torrentify
+
+go 1.18
+
+require (
+	github.com/anacrolix/tagflag v1.3.0
+	github.com/anacrolix/torrent v1.42.0
+)
+
+require (
+	github.com/anacrolix/missinggo v1.3.0 // indirect
+	github.com/anacrolix/missinggo/v2 v2.5.4-0.20220317032254-8c5ea4947a0b // indirect
+	github.com/bradfitz/iter v0.0.0-20191230175014-e8f45d346db8 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.10.3 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	_ "embed"
+	"github.com/anacrolix/torrent/bencode"
+	"github.com/anacrolix/torrent/metainfo"
+	"github.com/pkg/errors"
+	"github.com/urfave/cli/v2"
+	"io"
+	"log"
+	"os"
+)
+
+//go:embed version.txt
+var version string
+
+type torrent struct {
+	AnnounceUrls []string
+	Comment      string
+	CreatedBy    string
+	Name         string
+	Private      bool
+	Root         string
+}
+
+func main() {
+	app := &cli.App{
+		Name:        "torrentify",
+		Usage:       "torrent creator",
+		ArgsUsage:   "<torrent root>",
+		Version:     version,
+		Description: "torrentify creates torrent files from a directory.",
+		Flags: []cli.Flag{
+			&cli.StringSliceFlag{
+				Name:     "announce",
+				Aliases:  []string{"a"},
+				Usage:    "tracker announce urls",
+				EnvVars:  []string{"ANNOUNCE_URL"},
+				Required: true,
+			},
+			&cli.PathFlag{
+				Name:     "output",
+				Aliases:  []string{"o"},
+				Usage:    "torrent file path",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:  "comment",
+				Usage: "torrent comment",
+			},
+			&cli.StringFlag{
+				Name:    "createdby",
+				Usage:   "torrent creator name",
+				EnvVars: []string{"CREATED_BY"},
+			},
+			&cli.BoolFlag{
+				Name:    "private",
+				Usage:   "set torrent as private",
+				EnvVars: []string{"PRIVATE"},
+			},
+		},
+		HideHelpCommand: true,
+		Action: func(ctx *cli.Context) error {
+			if ctx.Args().Len() != 1 {
+				return cli.ShowAppHelp(ctx)
+			}
+
+			t := &torrent{
+				AnnounceUrls: ctx.StringSlice("announce"),
+				Comment:      ctx.String("comment"),
+				CreatedBy:    ctx.String("createdby"),
+				Name:         ctx.String("name"),
+				Private:      ctx.Bool("private"),
+				Root:         ctx.Args().Get(0),
+			}
+
+			outputPath := ctx.Path("output")
+			var w io.Writer
+			if outputPath == "-" {
+				w = os.Stdout
+			} else {
+				f, err := os.Create(outputPath)
+				if err != nil {
+					return errors.Wrap(err, "create file")
+				}
+				w = f
+			}
+
+			return makeTorrent(t, w)
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func makeTorrent(t *torrent, w io.Writer) error {
+	mi := metainfo.MetaInfo{
+		AnnounceList: make([][]string, 0),
+	}
+	for _, a := range t.AnnounceUrls {
+		mi.AnnounceList = append(mi.AnnounceList, []string{a})
+	}
+	mi.SetDefaults()
+	if len(t.Comment) > 0 {
+		mi.Comment = t.Comment
+	}
+	if len(t.CreatedBy) > 0 {
+		mi.CreatedBy = t.CreatedBy
+	}
+	info := metainfo.Info{
+		PieceLength: 1024 * 1024,
+		Private:     &t.Private,
+	}
+	err := info.BuildFromFilePath(t.Root)
+	if err != nil {
+		return errors.Wrap(err, "hash files")
+	}
+	if t.Name != "" {
+		info.Name = t.Name
+	}
+	mi.InfoBytes, err = bencode.Marshal(info)
+	if err != nil {
+		return errors.Wrap(err, "marshall torrent")
+	}
+
+	return mi.Write(w)
+}
